2e74a4b5c7c66faedb0950f46cf8d2ac395b073a
Sricharan <renish1419@gmail.com>
2024-10-16 16:53:48 +0530

    first commit

---
+++ run.py
@@ -0,0 +1,111 @@
+# Copyright 2023 The Distilling-step-by-step authors
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     https://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os 
+os.environ["WANDB_DISABLED"] = "true"
+import argparse
+
+from datasets import DatasetDict, concatenate_datasets
+from transformers import AutoTokenizer
+
+
+from data_utils import CQADatasetLoader, PYTHONDatasetLoader, SVAMPDatasetLoader, ESNLIDatasetLoader, ANLI1DatasetLoader, ASDivDatasetLoader
+from metrics import compute_metrics_text, compute_metrics_equation, compute_metrics_text_aux, compute_metrics_equation_aux
+from train_utils import train_and_evaluate
+
+def run(args):
+    #### Prepare datasets
+    if args.dataset == 'cqa':
+        dataset_loader = CQADatasetLoader()
+    elif args.dataset == 'python':
+        dataset_loader = PYTHONDatasetLoader()
+    else:
+        raise ValueError
+
+
+    datasets = dataset_loader.load_from_json()
+
+    if args.subsample < 1.0:
+        datasets['train'] = datasets['train'].train_test_split(test_size=1.0-args.subsample, seed=args.run)['train']
+
+    #### Prepare datasets Prepare data for training
+    tokenizer = AutoTokenizer.from_pretrained(args.from_pretrained)
+   
+
+    if args.model_type == 'task_prefix':
+        def tokenize_function(examples):
+            model_inputs = tokenizer(['predict: ' + text for text in examples['input']], max_length=args.max_input_length, truncation=True)
+            expl_model_inputs = tokenizer(['explain: ' + text for text in examples['input']], max_length=args.max_input_length, truncation=True)
+            model_inputs['expl_input_ids'] = expl_model_inputs['input_ids']
+            model_inputs['expl_attention_mask'] = expl_model_inputs['attention_mask']
+
+            with tokenizer.as_target_tokenizer():
+                label_output_encodings = tokenizer(examples['label'], max_length=32, truncation=True)
+                rationale_output_encodings = tokenizer(examples['rationale'], max_length=32, truncation=True)
+
+            model_inputs['labels'] = label_output_encodings['input_ids']
+            model_inputs['aux_labels'] = rationale_output_encodings['input_ids']
+
+            return model_inputs
+
+    tokenized_datasets = datasets.map(
+        tokenize_function,
+        remove_columns=['input', 'rationale', 'label'],
+        batched=True
+    )
+
+
+    if args.model_type == 'standard':
+        if args.dataset not in ['svamp', 'asdiv']:
+            compute_metrics = compute_metrics_text_aux(tokenizer)
+        else:
+            compute_metrics = compute_metrics_equation_aux(tokenizer)
+
+    else:
+        if args.dataset not in ['svamp', 'asdiv']:
+            compute_metrics = compute_metrics_text(tokenizer)
+        else:
+            compute_metrics = compute_metrics_equation(tokenizer)
+
+
+    train_and_evaluate(args, args.run, tokenizer, tokenized_datasets, compute_metrics)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset', type=str, required=True)
+    parser.add_argument('--subsample', type=float, default=1.0)
+    parser.add_argument('--alpha', type=float, default=0.5)
+    parser.add_argument('--max_steps', type=int, default=20000)
+    parser.add_argument('--eval_steps', type=int, default=1449)
+    parser.add_argument('--batch_size', type=int, default=64)
+    parser.add_argument('--optimizer_name', type=str, default='AdamW')
+    parser.add_argument('--lr', type=float, default=5e-5)
+    parser.add_argument('--run', type=int, default=0)
+    parser.add_argument('--from_pretrained', type=str, default='google/t5-v1_1-base')
+    parser.add_argument('--label_type', type=str, default='gt')
+    parser.add_argument('--llm', type=str, default=None)
+    parser.add_argument('--max_input_length', type=int, default=200)
+    parser.add_argument('--grad_steps', type=int, default=1)
+    parser.add_argument('--local_rank', type=int, default=-1)
+    parser.add_argument('--gen_max_len', type=int, default=32)
+    parser.add_argument('--parallelize', action='store_true')
+    parser.add_argument('--model_type', type=str, default='task_prefix')
+    parser.add_argument('--bf16', action='store_true')
+    parser.add_argument('--no_log', action='store_true')
+    parser.add_argument('--output_rationale', action='store_true')
+
+    args = parser.parse_args()
+
+    run(args)
